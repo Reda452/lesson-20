dbaa227a1be974f81b665004b771d52f58a9cc3b
Reda452 <redasakhi0@gmail.com>
2025-02-04 19:34:41 +0100

    new1

---
+++ activity2.py
@@ -0,0 +1,15 @@
+# function to chek whether
+# first and last character of words match
+def match_words(words):
+    ctr = 0
+    lst= []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
+
+    print ("list or words with first and last character same\n ", lst)
+    return ctr
+
+count = match_words(['abc', 'cfc', 'xyz', '1221'])
+print("Number of words having first and last character same: ", count)

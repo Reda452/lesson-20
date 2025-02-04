598597e1d63af0ea5058cc192a458a39e3a2bdf1
Reda452 <redasakhi0@gmail.com>
2025-02-04 19:49:16 +0100

    new2

---
+++ activity3.py
@@ -0,0 +1,26 @@
+L = [4,5,1,2,9,7,10,8]
+print ("original list: ", L)
+
+#variable to store the sum of
+# the list
+count = 0
+
+#finding the sum
+for i in L:
+    count += i
+
+#divide the total elements by
+# number of elements
+avg = count / len(L)
+
+print("sum = ", count)
+print("average = ", avg)
+
+#sorting the elements of the list
+L.sort()
+
+# printing the first element
+print("smallest element = ", L[0])
+
+#printing the last element
+print("largest element = ", L[-1])
